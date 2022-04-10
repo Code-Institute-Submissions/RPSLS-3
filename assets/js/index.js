9c988bff0da62adbdea27d6a91f798d02ccd8453
Simon 'Cooper' Coupe <Simonjosephcoupe@gmail.com>
2022-04-10 23:59:58 +0000

    Add dictionary to js for user picks

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,26 @@
 
 
+const handOptions = {
+    "rock": "assets/images/Rock.png",
+    "paper": "/assets/images/Paper.png", 
+    "scissors": "/assets/images/Scissors.png",
+    "lizard": "/assets/images/Lizard.png",
+    "spock": "/assets/images/Spock.png"
+}
+
 const pickUserHand = (hand) => {
     console.log(hand);
-    // hide current page and show the next page with your choice and the computers choice
+    // hide current page when user makes choice
     let hands = document.querySelector(".hands");
     hands.style.display = "none";
+    
+    // show the win/lose/draw page when user makes choice
+    let contest = document.querySelector(".contest");
+    contest.style.display = "flex";
+
+    //show correct choice on win/lose/draw section when user choice made
+    document.getElementById("userPickImage").src = handOptions[hand];
+
+
+
 }
