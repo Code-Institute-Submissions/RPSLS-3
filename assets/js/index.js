5c162ccd61c9fa37bfb8cbe52e3e7ce33fdc3ca2
Simon 'Cooper' Coupe <Simonjosephcoupe@gmail.com>
2022-04-10 22:57:49 +0000

    Add functionality to remove the hands div and it's contents when the user clicks and makes a selection

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,8 @@
 
 
 const pickUserHand = (hand) => {
-    console.log(hand)
+    console.log(hand);
+    // hide current page and show the next page with your choice and the computers choice
+    let hands = document.querySelector(".hands");
+    hands.style.display = "none";
 }
